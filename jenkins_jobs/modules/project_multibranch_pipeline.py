799b8afaa4c9904f3cbf96b4124744486ba40020
Matt Drees <matt.drees@cru.org>
2018-02-05 11:48:05 -0700

    add multibranch pipeline project module skeleton

---
+++ jenkins_jobs/modules/project_multibranch_pipeline.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# Based on jenkins_jobs/modules/project_pipeline.py by
+# Copyright (C) 2016 Mirantis, Inc.
+#
+# The above (project_pipeline.py) was based on
+# jenkins_jobs/modules/project_workflow.py by
+# Copyright (C) 2015 David Caro <david@dcaro.es>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+"""
+The Multibranch Pipeline Project module handles creating
+Multibranch Jenkins Pipeline projects.
+You may specify ``multibranch-pipeline`` in the ``project-type`` attribute of
+the :ref:`Job` definition.
+
+
+"""
+import xml.etree.ElementTree as XML
+
+import jenkins_jobs.modules.base
+
+
+class MultibranchPipeline(jenkins_jobs.modules.base.Base):
+    sequence = 0
+
+    def root_xml(self, data):
+        xml_parent = XML.Element(
+            'org.jenkinsci.plugins.workflow.multibranch.'
+            'WorkflowMultiBranchProject',
+            {'plugin': 'workflow-multibranch'}
+        )
+
+        return xml_parent
